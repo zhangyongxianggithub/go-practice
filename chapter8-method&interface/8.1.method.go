bdc63d79ee12e1a0ea6914aa9bc5843a43c6e231
zhangyongxiang <1306835795@qq.com>
2021-04-26 23:49:43 +0800

    update

---
+++ chapter8-method&interface/8.1.method.go
@@ -0,0 +1,27 @@
+//@program: goPractice
+//@author: zhangyongxiang(zyxfox@foxmail.com)
+//@create: 2021-04-26 02:26
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Movie struct {
+	Name   string
+	Rating float64
+}
+
+func (m *Movie) summary() string {
+	r := strconv.FormatFloat(m.Rating, 'f', 1, 64)
+	fmt.Println(r)
+	return m.Name + ", " + r
+}
+func main() {
+	m := Movie{
+		Name:   "Spiderman",
+		Rating: 3.2,
+	}
+	fmt.Println(m.summary())
+}
