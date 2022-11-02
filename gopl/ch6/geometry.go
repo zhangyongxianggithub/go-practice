796f1637ef1a1501573c1c786e6eed01fa93d288
zhangyongxiang <zhangyongxiang@baidu.com>
2022-11-02 16:35:49 +0800

    update

---
+++ gopl/ch6/geometry.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct{ X, Y float64 }
+
+// 这是一个普通的函数
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// 这是Point类型的方法，参数p是方法的接受者，直线距离
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	fmt.Println("函数调用:", Distance(p, q)) // "5" 函数调用
+	fmt.Println("方法调用:", p.Distance(q))  // 方法调用
+}
---
+++ gopl/ch7/bytecounter.go
@@ -0,0 +1,8 @@
+package ch7
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
