a2353eb00d904aeaa2cb3f0d875aacebcc67d968
zhangyongxiang <zhangyongxiang@baidu.com>
2022-11-03 17:14:53 +0800

    update

--- src/gopl/ch7/7.1.go
+++ src/gopl/ch7/7.1.go
@@ -7,7 +7,12 @@ import (
 )
 
 func main() {
+	// 计算单词数
 	var w mod.WordCounter
 	w.Write([]byte("hello word go"))
 	fmt.Println(w)
+	// 计算行数
+	var l mod.LineCounter
+	l.Write([]byte("hello\nword\ngo\n\n"))
+	fmt.Println(l)
 }
