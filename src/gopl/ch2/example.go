85c46349acc68288af842936b814c68bdd0c6404
zhangyongxiang <zhangyongxiang@baidu.com>
2023-02-09 10:59:02 +0800

    update

---
+++ src/gopl/ch2/example.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "hello, world"
+	fmt.Println(len(s))
+	fmt.Println(s[0], s[7])
+}
--- src/gopl/ch2/tempconv/tempconv.go
+++ src/gopl/ch2/tempconv/tempconv.go
@@ -13,6 +13,13 @@ const (
 	BoilingC      Celsius = 100
 )
 
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
 func (c Celsius) String() string {
 	return fmt.Sprintf("%gC", c)
 }
