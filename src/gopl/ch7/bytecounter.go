d518bb6913358214ac875174682147bb47b8cefd
zhangyongxiang <1306835795@qq.com>
2023-02-21 07:36:23 +0800

    update

--- src/gopl/ch7/bytecounter.go
+++ src/gopl/ch7/bytecounter.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
-
-	"gopl/ch7/mod"
 )
 
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
 func main() {
-	var c mod.ByteCounter
+	var c ByteCounter
 	c.Write([]byte("hello"))
 	fmt.Println(c)
 	c = 0
 	var name = "Dolly"
-	fmt.Fprintf(&c, "hello,%s", name)
+	fmt.Fprintf(&c, "hello, %s", name)
 	fmt.Println(c)
+
 }
