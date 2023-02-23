fa8675f1c5b46c3a6c71750f5c5f2528fc2ed399
zhangyongxiang <1306835795@qq.com>
2023-02-23 21:47:09 +0800

    update

---
+++ src/gopl/ch8/clock1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		handleConn(conn) // 一次处理一个连接
+
+	}
+
+}
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // 连接断开
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
