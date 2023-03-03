3fd0953b8423ea500e3718025ed3360651bf1d56
zhangyongxiang <zhangyongxiang@baidu.com>
2023-03-03 18:51:54 +0800

    update

---
+++ gopl/ch7/7.1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+
+    "go-practice/gopl/ch7/mod"
+)
+
+func main() {
+    // 计算单词数
+    var w mod.WordCounter
+    w.Write([]byte("hello word go"))
+    fmt.Println(w)
+    // 计算行数
+    var l mod.LineCounter
+    l.Write([]byte("hello\nword\ngo\n\n"))
+    fmt.Println(l)
+}
