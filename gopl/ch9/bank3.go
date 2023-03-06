c108eb8baf78b11149c6b6ffd60260dee7282b5c
zhangyongxiang <1306835795@qq.com>
2023-03-06 23:09:59 +0800

    update

---
+++ gopl/ch9/bank3.go
@@ -0,0 +1,22 @@
+package ch9
+
+import (
+	"sync"
+)
+
+var (
+	mu      sync.Mutex
+	balance int
+)
+
+func Deposit(amount int) {
+	mu.Lock()
+	balance += amount
+	mu.Unlock()
+}
+func Balance() int {
+	mu.Lock()
+	b := balance
+	mu.Unlock()
+	return b
+}
