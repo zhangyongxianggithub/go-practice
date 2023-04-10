fb1e54316323a565807fca92ef4220454584ff13
zhangyongxiang <zyx@bestzyx.com>
2023-04-10 13:22:28 +0800

    update

--- viper/viper.go
+++ viper/viper.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/spf13/viper"
 )
 
@@ -9,5 +11,14 @@ func main() {
 	viper.SetDefault("ContentDir", "content")
 	viper.SetDefault("LayoutDir", "layouts")
 	viper.SetDefault("Taxonomies", map[string]string{"tag": "tags", "category": "categories"})
+
+	viper.SetConfigName("config") // name of config.toml file (without extension)
+	// viper.AddConfigPath("/etc/appname/")  // path to look for the config.toml file in
+	// viper.AddConfigPath("$HOME/.appname") // call multiple times to add many search paths
+	viper.AddConfigPath(".")    // optionally look for config.toml in the working directory
+	err := viper.ReadInConfig() // Find and read the config.toml file
+	if err != nil {             // Handle errors reading the config.toml file
+		panic(fmt.Errorf("Fatal error config.toml file: %s \n", err))
+	}
 
 }
