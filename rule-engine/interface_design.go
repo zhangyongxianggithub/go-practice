9919e83009bb40810472d652ce80272c836a3eb8
zhangyongxiang <zhangyongxiang@baidu.com>
2023-06-05 11:26:48 +0800

    update

--- rule-engine/interface_design.go
+++ rule-engine/interface_design.go
@@ -7,10 +7,41 @@ type Source interface {
 
 	GetProperties() (map[string]string, error)
 }
+
 type GraphRule string
 
 type DAG string
 
 type GraphAnalyzer interface {
-	convertGraphRule(dag DAG) (GraphRule, error)
+	ConvertGraphRule(dag DAG) (GraphRule, error)
+}
+type DAGNode string
+
+type GraphNodeRule string
+
+type RuleNodeAnalyzer interface {
+	ConvertGraphNodeRule(node DAGNode) (GraphNodeRule, error)
+}
+
+type Sink interface {
+	ConvertGraphNodeRule(node DAGNode) (GraphNodeRule, error)
+
+	SilentSetting(silentSetting SilentSetting)
+}
+
+type SilentSetting interface {
+	// TODO: 定义支持的几种静默条件设置
+}
+
+type Type struct {
+}
+
+type Engine interface {
+	RegisterSource(source Source) error
+	RemoveSource(source Source) error
+	RegisterSink(sink Sink) error
+	RemoveSink(sink Sink) error
+	GetGraphAnalyzer(typo Type) GraphAnalyzer
+	GetRuleNodeAnalyzer(typo Type, nodeType string) RuleNodeAnalyzer
+	Submit(rule GraphRule)
 }
