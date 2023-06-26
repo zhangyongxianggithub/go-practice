65ef5cd37dd5987fe756820648a37f9a10e909ee
zhangyongxiang <zyx@bestzyx.com>
2023-06-26 13:02:20 +0800

    update

--- gorm/hasmany.go
+++ gorm/hasmany.go
@@ -13,31 +13,32 @@ import (
 */
 // User 有多张 CreditCard，UserID 是外键
 type User struct {
-	gorm.Model
-	CreditCards []CreditCard `gorm:"foreignKey:UserRefer;"`
+	// gorm.Model
+	Name        string        `gorm:"size:128;primaryKey"`
+	CreditCards []*CreditCard `gorm:"foreignKey:UserRefer;references:Name"`
 }
 
 type CreditCard struct {
 	gorm.Model
 	Number    string
-	UserRefer uint
+	UserRefer string `gorm:"size:128"`
 }
 
 func main() {
 
-	dsn := "root:163766@tcp(a.bestzyx.com:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:bigdataNV7U@tcp(10.162.195.168:8270)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 	db.AutoMigrate(&User{}, &CreditCard{})
-	// db.Create(&User{Model: gorm.Model{
-	// 	ID: 1,
-	// }})
+	// db.Create(&User{Name: "zyx", CreditCards: []*CreditCard{{
+	// 	Model:  gorm.Model{},
+	// 	Number: "wqewqewqewq",
+	// }}})
 	users, _ := GetAll(db)
 	str, _ := json.Marshal(users)
 	fmt.Printf("%s", str)
-
 }
 
 // 检索用户列表并预加载信用卡
