83cde2d77c5b66b2c463b8b1a3da6045b5d62c5e
zhangyongxiang <zyx@bestzyx.com>
2023-07-13 22:18:30 +0800

    update

--- channel/defer.go
+++ channel/defer.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/url"
 )
 
 func main() {
+	var r io.Reader
 	defer func() {
+		r.Read(make([]byte, 1024))
 		fmt.Println("first defer")
 	}()
+	fmt.Println("main")
 	defer func() {
 		fmt.Println("second defer")
 	}()
