445aee9e436864dbe1914f1f9769a46c7da6b314
zhangyongxiang <1306835795@qq.com>
2024-04-24 15:38:31 +0800

    update

--- viper/viper_write_config.go
+++ viper/viper_write_config.go
@@ -3,13 +3,15 @@ package main
 import "github.com/spf13/viper"
 
 func main() { // 指定配置文件路径
-	viper.SetConfigName("config1") // 配置文件名称(无扩展名)
+	viper.SetConfigName("config") // 配置文件名称(无扩展名)
+	viper.SetConfigType("json")
 	viper.AddConfigPath("./viper") // 多次调用以添加多个搜索路径
 	viper.AddConfigPath(".")
-	viper.Set("format", "write")
-	viper.WriteConfig() // 将当前配置写入“viper.AddConfigPath()”和“viper.SetConfigName”设置的预定义路径
-	// viper.SafeWriteConfig()
+	viper.Set("format", "json-rewrite")
+	viper.Set("format1", "json-rewrite")
+	// viper.WriteConfig() // 将当前配置写入“viper.AddConfigPath()”和“viper.SetConfigName”设置的预定义路径
+	viper.SafeWriteConfig()
 	// viper.WriteConfigAs("./config-as")
-	// viper.SafeWriteConfigAs("./config-as") // 因为该配置文件写入过，所以会报错
-	// viper.SafeWriteConfigAs("./config-as-other")
+	//viper.SafeWriteConfigAs("./config-as") // 因为该配置文件写入过，所以会报错
+	//viper.SafeWriteConfigAs("./config-as-other")
 }
